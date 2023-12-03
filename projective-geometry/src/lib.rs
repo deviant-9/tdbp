c6033c868091970b5a538cbcd4cb99ed7dcdb42f
Dmitry Alexeyev (aka deviant_9) <alexeyev.d@mail.ru>
2023-12-03 19:24:16 +0300

    rustfmt

--- projective-geometry/src/lib.rs
+++ projective-geometry/src/lib.rs
@@ -4,13 +4,13 @@
 #![feature(slice_flatten)]
 #![feature(slice_as_chunks)]
 
-pub mod array_utils;
-pub mod scalar_traits;
-pub mod tensors;
-pub mod levi_civita;
 pub mod adjugate;
+pub mod array_utils;
+pub mod camera;
 pub mod eigen_vectors;
 pub mod homogeneous_equations;
-pub mod projective_primitives;
-pub mod camera;
 pub mod homography;
+pub mod levi_civita;
+pub mod projective_primitives;
+pub mod scalar_traits;
+pub mod tensors;
